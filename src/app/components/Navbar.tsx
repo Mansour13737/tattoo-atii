3d7020c90cd54ea4350e9272c85dab6be016390d
Mansour13737 <mansournosrati69@gmail.com>
2025-06-30 02:09:57 +0330

    6

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <nav className="w-full flex justify-between px-4 py-4 border-[0.5px] border-white/10 text-[#d1b2a0] tracking-[0.5px]">
+        <nav className="w-full bg-gray-950 flex justify-between px-4 py-4 border-[0.5px] border-white/10 text-[#d1b2a0] tracking-[0.5px]">
             <div>
                 <span className="tracking-[5px]"><span className="text-[#976e4f] font-bold">E</span>LARA</span>
             </div>
--- src/app/components/Text.tsx
+++ src/app/components/Text.tsx
@@ -1,8 +1,8 @@
 
 export default function Text() {
     return (
-        <div className="absolute bottom-2 text-[#d1b2a0] text-[11px] w-[80%] px-3 ">
-            <p>Designed for women. Inspired by art. Created with passion.
+        <div className="absolute bottom-2 w-full bg-black ">
+            <p className="text-[#d1b2a0] text-[11px] w-[80%] px-3 h-full ">Designed for women. Inspired by art. Created with passion.
                 © 2025 — All rights reserved.
             </p>
         </div>
