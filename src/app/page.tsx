774c4988a72535ec5e3ace6fa7de7a0754d0e2fe
Mansour13737 <mansournosrati69@gmail.com>
2025-06-30 01:53:14 +0330

    5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import Text from "./components/Text";
 
 export default function Home() {
   return (
-      <div className="w-full relative h-screen bg-[url('/bg-home.jpeg')] bg-center bg-no-repeat bg-contain flex flex-col ">
+      <div className="w-full relative h-screen bg-[url('/bg-home.jpeg')] bg-center bg-no-repeat bg-cover flex flex-col">
         <Navbar/>
-        <Link href={'/'} className="text-[8px] font-extralight  mt-[65%] ml-12 border-[#d1b2a0]/70 border-[0.8px] px-[20px] py-[8px] w-fit">VIEW GALLERY</Link>
+        <Link href={'/'} className="text-[8px] absolute font-extralight  mt-[80%] ml-12 border-[#d1b2a0]/70 border-[0.8px] px-[20px] py-[8px] w-fit">VIEW GALLERY</Link>
         <Footer/>
         <Text />
       </div>
