c38728153519a6df2a29bee6c726f80522320262
Mansour13737 <mansournosrati69@gmail.com>
2025-06-30 04:09:23 +0330

    7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Merriweather, Playfair_Display } from "next/font/google";
+import { Merriweather, Playfair_Display } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const merri = Merriweather({
   subsets: ['latin'],
