2272f10bcfff5a3e4d668579a7eb3918b81d9878
Mansour13737 <mansournosrati69@gmail.com>
2025-07-02 21:30:29 +0330

    9

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link"
+type NavText = {
+    left? : string;
+    right? : string;
+}
+export default function Navbar({left,right} : NavText) {
 
-export default function Navbar() {
     return (
-        <nav className="w-full font-display flex justify-between px-2 md:px-3 lg:px-4 py-2 md:py-3 lg:py-4 border-[0.5px] border-white/10 text-[#d1b2a0] tracking-[0.3px] md:tracking-[0.4px] lg:tracking-[0.5px]">
+        <nav className={`w-full font-display flex justify-between px-2 md:px-3 lg:px-4 py-2 md:py-3 lg:py-4 border-b-[0.5px] border-white/40 backdrop-blur-[2px]  tracking-[0.3px] md:tracking-[0.4px] lg:tracking-[0.5px]`}>
             <div>
-                <span className="tracking-[3px] md:tracking-[4px] lg:tracking-[5px] text-[16px] md:text-base lg:text-lg"><span className="text-[#976e4f] font-bold">E</span>LARA</span>
+                <span className={`tracking-[3px] lg:text-[25px] ${left} md:tracking-[4px] lg:tracking-[5px] text-[18px] md:text-base`}><span className="text-[#976e4f] font-bold">E</span>LARA</span>
             </div>
-            <ul className="flex justify-around gap-4 md:gap-3 lg:gap-5 text-[11px] font-extralight md:text-[11px] lg:text-[12px] items-center leading-6">
-                <Link href={'/gallery'} className="hover:text-[#976e4f] transition-colors">GALLERY</Link>
-                <Link href={'/'} className="hover:text-[#976e4f] transition-colors">ABOUT</Link>
-                <Link href={'/'} className="hover:text-[#976e4f] transition-colors">BLOG</Link>
+            <ul className={`flex ${right} justify-around gap-4 md:gap-3 lg:gap-8 text-[12px] font-extralight md:text-[11px] lg:text-[14px] items-center leading-6  `}>
+                <Link href={'/gallery'} className="hover:text-[#FFFF] transition-colors">GALLERY</Link>
+                <Link href={'/'} className="hover:text-[#FFFF] transition-colors">ABOUT</Link>
+                <Link href={'/'} className="hover:text-[#FFFF] transition-colors">BLOG</Link>
             </ul>
         </nav>
     )
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,8 +1,33 @@
 import Image from "next/image"
+import Link from "next/link"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+import Text from "../components/Text"
+
 export default function Gallery() {
-  return (
-    <div className="w-full flex justify-center items-center min-h-screen bg-[url('/image.png')] bg-no-repeat bg-center bg-cover">
-      <Image  src={'/gallery.png'} width={400} height={500} alt="pic" className="w-[100vw] h-[92vh]"/>
-    </div>
-  )
+    return (
+        <div className="w-full h-screen relative flex flex-col  justify-start items-center min-h-screen bg-[url('/image.png')] bg-no-repeat bg-center bg-cover">
+            <Navbar left="text-[#231004]" right="text-[#231004]" />
+            <p className="text-center tracking-wide mt-[5%] text-[17px] font-semibold text-black font-display">A Curated Collection of Timeless & <br /> Modern Tattoos For Women <br />
+                Who Seek Meaning, Beauty, and Elegance</p>
+            <ul className="flex gap-4 text-[12px] mt-5 tracking-wide font-display">
+                <li>CLASSIC</li>
+                <li>MINIMAL</li>
+                <li>MODERN</li>
+                <li>FANTASY</li>
+            </ul>
+            <div className="grid grid-cols-3 gap-1 mt-2 gap-x-1 ">
+                <Image src={'/tattoos/1.png'} width={100} height={100} alt="pic"/>
+                <Image src={'/tattoos/2.png'} width={100} height={100} alt="pic" className="h-[118px]"/>
+                <Image src={'/tattoos/3.png'} width={100} height={100} alt="pic" className="h-[118px]"/>
+                <Image src={'/tattoos/4.png'} width={100} height={100} alt="pic"/>
+                <Image src={'/tattoos/5.png'} width={100} height={100} alt="pic"/>
+                <Image src={'/tattoos/6.png'} width={100} height={100} alt="pic" className="h-[118px]"/>
+                <Image src={'/tattoos/7.png'} width={100} height={100} alt="pic"/>
+            </div>
+            <Link href={'/'} className="text-[10px] h-fit text-white md:text-[10px] lg:text-[16px] absolute lg:left-0 lg:-top-[90%] lg:font-extralight font-extralight md:mt-[70%] lg:mt-[80%] md:ml-8 lg:ml-12 border-[#231004]/70 border-[0.8px] px-4 md:px-4 lg:px-[20px] py-2 md:py-3 lg:py-[8px] w-fit left-12 top-[81%] lg:hover:bg-[rgba(0,0,0,0.1)] font-display ">BACK</Link>
+            <Footer />
+            <Text />
+        </div>
+    )
 }
